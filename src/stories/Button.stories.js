23c8438d84f9e1f0c3732c4b9564f983403d1b2a
Cromeror <cristobalromerorossi@gmail.com>
2022-08-04 10:20:57 -0500

    chore: updated storybook examples

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -3,42 +3,53 @@ import { Button } from "./Button";
 // More on default export:
 // https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
 export default {
-  title: "Example/Button",
+  title: "Components/Button",
   // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
   argTypes: {
-    backgroundColor: { control: "color" },
     onClick: { action: "onClick" },
     size: {
       control: { type: "select" },
-      options: ["small", "medium", "large"],
+      options: ["small", "medium", "big"],
+    },
+    color: {
+      control: { type: "select" },
+      defaultValue: "green",
+      options: ["green", "grey", "brown"],
+    },
+    mode: {
+      control: { type: "select" },
+      defaultValue: "primary",
+      options: ["primary", "secondary", "link"],
     },
   },
 };
 
 // More on component templates:
 // https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
-const Template = (args) => Button(args);
+const Template = (args) => {
+  let newArgs = { ...args };
+  if (args.mode === "link") {
+    newArgs.color = "green";
+  }
+
+  return Button(newArgs);
+};
+
+const commonArgs = {
+  disabled: false,
+  rounded: false,
+  label: "Doppler button",
+};
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: "Button",
+  ...commonArgs,
+  loading: false,
+  preloading: false,
 };
 
 export const Secondary = Template.bind({});
-Secondary.args = {
-  label: "Button",
-};
+Secondary.args = { ...commonArgs, mode: "secondary" };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: "large",
-  label: "Button",
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: "small",
-  label: "Button",
-};
+export const Link = Template.bind({});
+Link.args = { ...commonArgs, mode: "link" };
