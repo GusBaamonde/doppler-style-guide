f904481cfce974413ae4116835c47f30804d8b09
GusBaamonde <gusvodesign@gmail.com>
2023-08-30 18:17:44 -0300

    fix: fix in message error inputs in contact policy

---
+++ src/stories/Contact.Policy.stories.js
@@ -0,0 +1,22 @@
+import { Inputs } from "./Contact.Policy";
+
+// More on default export:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: "Components/Contact.Policy",
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = (args) => {
+  return Inputs(args);
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  isError: false,
+  label: "Assisted text for field validations in this component",
+};
