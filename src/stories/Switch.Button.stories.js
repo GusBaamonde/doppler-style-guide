5f531d4e19ad815656213a1380887c1e990a3ba9
GusBaamonde <gusvodesign@gmail.com>
2023-09-13 18:00:00 -0300

    fix: add new disabled state to switch buttons

---
+++ src/stories/Switch.Button.stories.js
@@ -0,0 +1,18 @@
+import { Inputs } from "./Switch.Button";
+
+// More on default export:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: "Components/Switch.Button",
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = (args) => {
+  return Inputs(args);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
