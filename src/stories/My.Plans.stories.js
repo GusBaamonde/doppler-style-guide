409df6c39a68085ca9946fa510f2cf46dc59fb9c
GusBaamonde <gusvodesign@gmail.com>
2024-05-28 12:37:47 -0300

    feat: add new page my plan layout

---
+++ src/stories/My.Plans.stories.js
@@ -0,0 +1,18 @@
+import { Inputs } from "./My.Plans";
+
+// More on default export:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: "Components/My.Plans",
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+  argTypes: {},
+};
+
+// More on component templates:
+// https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template = (args) => {
+  return Inputs(args);
+};
+
+export const Default = Template.bind({});
+Default.args = {};
